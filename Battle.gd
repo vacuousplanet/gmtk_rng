6074437978bdd5715be886ef2fafcadaf28de91a
Tanner Samples <83928312+TS-Programming@users.noreply.github.com>
2023-07-09 00:48:47 -0700

    Update Battle.gd
    wrote code in battle

--- Battle.gd
+++ Battle.gd
@@ -5,8 +5,10 @@ const fork_scene = preload("res://fork.tscn")
 
 var item_deck = {"fork": fork_scene}
 
+var start_battle = false
 
-
+var fighter = []
+var current_fighter = 0
 
 var hero = hero_scene.instance()
 var fork = fork_scene.instance()
@@ -14,15 +16,28 @@ var fork = fork_scene.instance()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
-	var p1 = {"health": hero.health, "attack": fork.attack, "defense": fork.defense}
-
+	start_battle = true
 	pass # Replace with function body.
 	
 func init_battle(hero, enemy):
-	
+	fighter.clear()
+	fighter.append(hero)
+	fighter.append(enemy)
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	if(start_battle):
+		init_battle(draw_item(), draw_item())
+		#fighters[][0] is the fighters health
+		while(fighter[current_fighter][0] > 0):
+			print("ok")
+			
+			
+		start_battle = false
 	pass
+	
+func draw_item():
+	return  {"health": hero.health, "attack": fork.attack, "defense": fork.defense}
